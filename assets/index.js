6be9ef6c07aa698201cb99b8d3908f40a11d12de
DiallAbdoulaye <diallo.a6@gmail.com>
2018-05-04 16:50:49 +0200

    ajout du projet

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+export const CONFIG = {
+    STORAGE: {
+      AVAILABLE_REGIONS: '@availabe_regions',
+      CURRENT_REGION: '@current_region'
+    },
+    YOUTUBE: {
+      BASE_URL: 'https://www.googleapis.com/youtube/v3',
+      API_KEY: 'AIzaSyCuIylDihlQ_ornVnryquG8rpEeMjhwjiI',
+      DEFAULT_REGION: 'FR',
+      DEFAULT_NB_RESULT: 20
+    }
+  }
